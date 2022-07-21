24e6a40970b485abb93b76263ea420bcb4a3844b
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-20 23:15:27 -0300

    Conserta foto Lari

--- src/Componentes/bio/Bio.jsx
+++ src/Componentes/bio/Bio.jsx
@@ -1,10 +1,11 @@
 import './Bio.css'
+import foto from '../../assets/foto_lari.jpg'
 
 function Bio() {
     return (
         <>
             <div className="bio">
-                <img className="foto-bio" src="foto_lari.jpg" />
+                <img className="foto-bio" src={foto} />
                 <div>
                     <h2>Oi! Eu sou a Larissa</h2>
                     <p>Pra quem não me conhece, tenho 24 anos e sou formada em nutrição pela USP. Meu signo é Capricórnio, gosto muito de ler, testar receitinhas, sair com os amigos e com a família e praticar Jump. Recentemente, comecei a programar front-end pela reprograma.</p>
